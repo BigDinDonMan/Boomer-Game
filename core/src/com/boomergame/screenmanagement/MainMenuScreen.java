403132a14141e4981aac2e6a13ceeca9ffdbf078
Maciej <gorski.maciej@o2.pl>
2020-06-23 22:59:35 +0200

    added new game screens and dispose methods to them, also moved logic and resources from game class to screen classes

--- core/src/com/boomergame/screenmanagement/MainMenuScreen.java
+++ core/src/com/boomergame/screenmanagement/MainMenuScreen.java
@@ -1,26 +1,56 @@
 package com.boomergame.screenmanagement;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.mygdx.game.BoomerGame;
 
 public class MainMenuScreen implements Screen {
 
-    BoomerGame game;
+    Stage stage;
+    SpriteBatch batch;
+    ExtendViewport viewport;
+    OrthographicCamera camera;
+    TextureRegion buttonTextureRegion;
+    TextureRegionDrawable drawableButtonRegion;
+    Texture buttonTexture;
+    Texture backgroundTexture;
 
-    public MainMenuScreen(BoomerGame game) {
-        this.game = game;
+    public MainMenuScreen() {
+        this.batch = new SpriteBatch();
+        this.camera = new OrthographicCamera();
+        this.viewport = new ExtendViewport(1280, 800, this.camera);
+        this.stage = new Stage(this.viewport, this.batch);
+        this.backgroundTexture = new Texture("main_background.jpg");
     }
-
-
 
     @Override
     public void show() {
+        Table mainTable = new Table();
+        mainTable.setFillParent(true);
+        mainTable.top();
 
+        this.stage.addActor(mainTable);
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        this.stage.act(Gdx.graphics.getDeltaTime());
+        this.stage.draw();
     }
 
     @Override
@@ -45,6 +75,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        this.buttonTexture.dispose();
+        this.backgroundTexture.dispose();
     }
 }
--- core/src/com/mygdx/game/BoomerGame.java
+++ core/src/com/mygdx/game/BoomerGame.java
@@ -1,78 +1,52 @@
 package com.mygdx.game;
 
-import com.badlogic.ashley.core.Engine;
-import com.badlogic.ashley.core.Entity;
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2D;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.boomergame.core.GameConstants;
-import com.boomergame.systems.DisposableSystem;
+import com.boomergame.screenmanagement.*;
 
-import java.util.ArrayList;
-import java.util.List;
+public class BoomerGame extends Game {
 
-public class BoomerGame extends ApplicationAdapter {
 
-	Engine ECSEngine;
-	World world;
-
-	List<DisposableSystem> systemsForCleanup;
-
-	Entity playerEntity;
-
-    //<editor-fold> Game resources (init in create();)
-	SpriteBatch mainBatch;
-	SpriteBatch UIBatch;
+	public SpriteBatch mainBatch;
+	public SpriteBatch UIBatch;
 	Texture img;
-	OrthographicCamera gameCamera;
-	ExtendViewport viewport;
-	//</editor-fold>
+
+
+	MainMenuScreen menuScreen;
+	OptionsScreen optionsScreen;
+	MainGameScreen gameScreen;
+	GameShopScreen shopScreen;
+	CreditsScreen creditsScreen;
 
 	public BoomerGame() {
 		super();
-		this.ECSEngine = new Engine();
-		this.world = new World(new Vector2(0, -10), true);
-		this.playerEntity = new Entity();
-		this.ECSEngine.addEntity(this.playerEntity);
-		this.systemsForCleanup = new ArrayList<>();
 	}
+
+	private void createScreens() {
+        this.menuScreen = new MainMenuScreen();
+        this.optionsScreen = new OptionsScreen();
+        this.gameScreen = new MainGameScreen();
+        this.shopScreen = new GameShopScreen();
+        this.creditsScreen = new CreditsScreen();
+    }
 
 	@Override
 	public void create () {
 	    Box2D.init();
 	    this.initializeGameResources();
+	    this.createScreens();
+	    this.setScreen(this.menuScreen);
 	}
 
 	@Override
 	public void render () {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        this.stepWorld();
-        this.ECSEngine.update(Gdx.graphics.getDeltaTime());
 	}
-
-	private float accumulator = 0;
-
-	private void stepWorld() {
-	    float delta = Gdx.graphics.getDeltaTime();
-	    accumulator += Math.min(delta, 0.25f);
-	    if (accumulator >= GameConstants.PHYSICS_STEP_TIME) {
-	        accumulator -= GameConstants.PHYSICS_STEP_TIME;
-            this.world.step(
-                    GameConstants.PHYSICS_STEP_TIME,
-                    GameConstants.VELOCITY_ITERATIONS,
-                    GameConstants.POSITION_ITERATIONS
-            );
-        }
-    }
 
 	@Override
 	public void dispose () {
@@ -81,32 +55,23 @@ public class BoomerGame extends ApplicationAdapter {
 
 	@Override
     public void resize(int x, int y) {
-        this.viewport.update(x, y, true);
-
-        this.mainBatch.setProjectionMatrix(this.gameCamera.combined);
     }
 
     private void performCleanup() {
         mainBatch.dispose();
         UIBatch.dispose();
         img.dispose();
-        this.disposeOfSystems();
-    }
 
-	private void disposeOfSystems() {
-        this.systemsForCleanup.forEach(DisposableSystem::dispose);
+        this.menuScreen.dispose();
+        this.creditsScreen.dispose();
+        this.shopScreen.dispose();
+        this.optionsScreen.dispose();
+        this.gameScreen.dispose();
     }
 
     private void initializeGameResources() {
         this.mainBatch = new SpriteBatch();
         this.UIBatch = new SpriteBatch();
         this.img = new Texture("badlogic.jpg");
-
-        this.gameCamera = new OrthographicCamera(1280, 800);
-        this.viewport = new ExtendViewport(
-                1280,
-                800,
-                this.gameCamera
-        );
     }
 }
